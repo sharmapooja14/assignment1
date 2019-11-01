7759da1a30e0037820f816ede6693d2a044422cd
sharmapooja14 <plsharma483@gmail.com>
2019-11-01 15:10:56 +0100

    modifiled update page

--- update.php
+++ update.php
@@ -7,24 +7,23 @@
      header('location: login.php');
  }
  
- if(isset($_POST['submit'])){
+ if(isset($_GET["id"])){
+    $displayquery = "SELECT * from `registration_form`  where id='".$_GET["id"]."'";
+    $query = mysqli_query($con,$displayquery); 
+    
+         $rows =  mysqli_fetch_array($query);
 
- $id = $_GET['id'];
 
-                    $username = $_POST['txt_uname'];
-                    $lastname = $_POST['lastname'];
-                    $image = $_FILES['image'];
-                    $dob = $_POST['dob'];
-                    $myFile = $_FILES['myFile'];
-                    $addresss = $_POST['address'];
-                    $country = $_POST['country'];
-                    $city = $_POST['city'];
- $q = " update registration_form set id=$id, fname='$username', lname='$lastname', pic='$image', dob='$dob',
- resume='$myFile', address='$addresss', country='$country', city='$city' where id=$id  ";
-
- $query = mysqli_query($con,$q);
-
- header('location:uploadrecord.php');
+                    $username =  $rows['fname'];
+                    $lastname =  $rows['lname'];
+                    $gender = $rows['gender'];
+                    $image = $rows['pic'];
+                    $dob = $rows['dob'];
+                   $resume = $rows['resume'];
+                    $address = $rows['address'];
+                    $hobbies = $rows['hobbies'];
+                    $country = $rows["country"];
+                    $city = $rows["city"];
  }
 
 ?>
@@ -83,41 +82,41 @@
             
                 <div class="form-group">
                     <label for="fristname"> First name: </label>
-                    <input type="text" class="form-control" id="txt_uname" name="txt_uname" placeholder="Frist Name"  />
+                    <input type="text" class="form-control" id="txt_uname" value="<?php if(isset($_GET["id"])){echo $username; } ?>" name="txt_uname" placeholder="Frist Name"  />
                 </div>
 
             <div>
                 <label for="lastname">Last name:</label>
-                <input type="text" id="txt_uname" name="lastname" placeholder="Last Name" class="form-control"/>
+                <input type="text" id="txt_uname" value="<?php if(isset($_GET["id"])){echo $lastname; } ?>" name="lastname" placeholder="Last Name" class="form-control"/>
                 
             </div>
 
             <div class="form-check-inline">
                  <label class="form-check-label">Gender: </label><br>
-                 <input type="radio"  name="optradio" value="male">Male
-                 <input type="radio" name="optradio" value="female">Female
+                 <input type="radio"  name="optradio" checked="checked" <?php if(isset($_GET["show"])){if($gender=="Male"){ echo"checked='checked'";}}?> value="Male">Male
+                 <input type="radio" name="optradio" <?php if(isset($_GET["show"])){if($gender=="Female"){ echo"checked='checked'";}}?> value="Female">Female
              </div>
 
             <div>
                 <label for="file">Profile Image:  </label>
-                <input type="file" name="image" class="form-control"><br><br>
+                <input type="file" value="<?php if(isset($_GET["id"])){echo $image; } ?>" name="image" class="form-control"><br><br>
             </div>
 
             <div>
                 <label for="dob">date Of Birth: </label>
-                <input type="date" class="form-control" id="" name="dob" placeholder="DOB"/>
+                <input type="date" class="form-control" id="" value="<?php if(isset($_GET["id"])){echo $dob; } ?>" name="dob" placeholder="DOB"/>
             </div>
 
             <div>
                 <label for="file">Select a Document:  </label>
-                <input type="file" name="myFile" class="form-control"><br><br>
+                <input type="file" value="<?php if(isset($_GET["id"])){echo $resume; } ?>" name="myFile" class="form-control"><br><br>
             </div>
 
             
 
             <div>
                 <label for="address">Address:  </label>
-                <textarea rows="4" cols="50" class="form-control" name="address">
+                <textarea rows="4" cols="50" class="form-control" name="address"> <?php if(isset($_GET["id"])){echo $address; } ?>
                
                 </textarea>
             </div>
@@ -126,8 +125,8 @@
                 <label for="country">Select Your Country</label>
                 <select id="input" name="country" class="form-control" onchange="random_function()">
                     <option>Select Your Country</option>
-                    <option>INDIA</option>
-                    <option>NORWAY</option>
+                    <option selected="selected" <?php if(isset($_GET["id"])){if($country=="INDIA"){ echo"selected='selected'";}}?>  value="INDIA">INDIA</option>
+                    <option selected="selected" <?php if(isset($_GET["id"])){if($country=="NORWAY"){ echo"selected='selected'";}}?>  value="INDIA">NORWAY</option>
                  </select>
                 <div id="output">
             </div>
@@ -135,9 +134,9 @@
             <div class="form-check">
                 <label class="form-check-label"> Hobbies </label><br>
                     
-                    <input type="checkbox" class="form-check-input" value="Reading books " name="chk">Reading books <br>
-                    <input type="checkbox" class="form-check-input" value="Cooking" name="chk">Cooking<br>
-                    <input type="checkbox" class="form-check-input" value="Driving" name="chk" >Driving<br>
+                    <input type="checkbox" class="form-check-input" selected="selected" <?php if(isset($_GET["id"])){if($hobbies=="Reading books"){ echo"selected='selected'";}}?> value="Reading books" name="chk[]" >Reading books <br>
+                    <input type="checkbox" class="form-check-input" selected="selected" <?php if(isset($_GET["id"])){if($hobbies=="Cooking"){ echo"selected='selected'";}}?> value="Cooking" name="chk[]">Cooking<br>
+                    <input type="checkbox" class="form-check-input" selected="selected" <?php if(isset($_GET["id"])){if($hobbies=="Driving"){ echo"selected='selected'";}}?> value="Driving" name="chk[]" >Driving<br>
               
             </div>
 <br>
@@ -150,5 +149,46 @@
     </div>
     
 </div> 
+<?php 
+    if(isset($_POST['submit'])){
+
+        $id = $_GET['id'];
+       
+                           $username = $_GET['txt_uname'];
+                           $lastname = $_POST['lastname'];
+                           $optradio = $_POST['optradio'];
+                           $image = $_FILES['image'];
+                           $dob = $_POST['dob'];
+                           $myFile = $_FILES['myFile'];
+                           $addresss = $_POST['address'];
+                           $country = $_POST['country'];
+                           $city = $_POST['any_name'];
+                           $hobbies = implode(",",$_POST['chk']);
+                           $imagename = $image['name'];
+                           $imageerror = $image['error'];
+                           $imagetemp = $image['tmp_name'];
+       
+                           $imageext = explode('.',$imagename);
+                         $imagechk = strtolower(end($imageext));
+       
+                           $iamgeextstroed = array('png','jpg','jpeg'); 
+                           
+                           $imageconvertintostring = implode('/',$iamgeextstroed);
+                           // echo $imageconvertintostring;
+                          
+                           if(in_array($imagechk,$iamgeextstroed)){
+                               
+       
+                               $destinationimage ='uploadimage/' .$imagename;
+                               move_uploaded_file($imagetemp,$destinationimage);
+        $q = " update registration_form set id=$id, fname='$username', lname='$lastname', gender='$optradio', pic='$imagename', dob='$dob',
+        resume='$imagename', address='$addresss', country='$country', city='$city', hobbies='$hobbies' where id=$id  ";
+       
+        $query = mysqli_query($con,$q);
+       
+        header('location:uploadrecord.php');
+        }
+    }
+?>
 </body>
 </html>
